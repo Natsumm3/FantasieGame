3cb8a413cb9ce94bd7fed1ee9727dda9671f7a5e
BennXYZ <sephirot5@web.de>
2016-11-14 16:44:18 +0100

    Framechecker
    kann bool abfragen, ob die gewünschte Framerate erreicht ist.
    
    zB
    while (window.IsOpen)
    if (FrameChecker.CheckFrame())
    {
    Stuff.exe
    }
    
    Vorher gewünschte Framerate setzen!

---
+++ FrameChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace FantasyGame
+{
+    class FrameChecker
+    {
+        public static Clock clock = new Clock();
+
+        public static int frameRate;
+
+        public static bool CheckFrame()
+        {
+            if(clock.ElapsedTime.AsSeconds() > (float) 1/frameRate)
+            {
+                clock.Restart();
+                return true;
+            }
+            return false;
+        }
+    }
+}
